11d0d1f642f7dc935399d3f250a2c0c9244544dc
naeglinghaff <kate.o.haffenden@gmail.com>
2020-02-24 16:55:11 +0000

    First tests for money

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,13 @@
 class Oystercard
-  
+
+  attr_reader :balance
+
+  def initialize
+    @balance = 0.00
+  end
+
+  def view_balance
+    @balance
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,11 @@
 require "oystercard"
 
 describe Oystercard do
+  before(:each) do
+    @card = Oystercard.new
+  end
 
+  it 'has money' do
+    expect(@card.view_balance).to be (@card.balance)
+  end
 end
